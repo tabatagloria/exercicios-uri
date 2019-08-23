f31b727767402eded6899df69a2bedbe87d156b1
tabatagloria <tabatabruna@yahoo.com.br>
2019-08-23 07:51:58 -0300

    Correção do exercício 1018

--- C/1018.c
+++ C/1018.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int i, n, notas, lista[7] = {100, 50, 20, 10, 5, 2, 1};
-    double quant;
+    int i, n, quantidadeNotas, notas[7] = {100, 50, 20, 10, 5, 2, 1};
+
 
     scanf("%d", &n);
     printf("%d\n", n);
 
     for(i = 0; i < 7; i++){
-        notas = n / lista[i];
-        n = n % lista[i];
-        quant = lista[i];
-        printf("%d notas(s) de R$ %.2lf\n", notas, quant);
+        quantidadeNotas = n / notas[i];
+        n = n % notas[i];
+        printf("%d nota(s) de R$ %d,00\n", quantidadeNotas, notas[i]);
     }
 
     return 0;
